6153c7f98f197ffbd1d3a4627b15447bf00cf9c8
narmitag <neil@neila.co.uk>
2015-02-20 10:32:40 +0000

    make the zip directory an option

--- manifests/installer.pp
+++ manifests/installer.pp
@@ -1,34 +1,34 @@
 class oracle::installer {
   include oracle::params
   include oracle::root_config
-  
+
   Exec { path => ['/usr/local/sbin', '/usr/sbin', '/sbin', '/usr/local/bin', '/usr/bin', '/bin'] }
-	
+
 	Class['oracle::root_config'] ->
-	
+
   file {'/app/oracle/install':
     owner => 'oracle',
     group => 'oinstall',
     mode => 0770,
     ensure => directory,
   } ->
-  
+
   exec {'unzip_installer':
-    command => 'unzip -o /vagrant/downloads/linux.x64_11gR2_database_1of2.zip -d /app/oracle/install; unzip -o /vagrant/downloads/linux.x64_11gR2_database_2of2.zip -d /app/oracle/install',
-    cwd => '/vagrant/downloads',
+    command => "unzip -o ${oracle::installFiles}/linux.x64_11gR2_database_1of2.zip -d /app/oracle/install; unzip -o ${oracle::installFiles}/linux.x64_11gR2_database_2of2.zip -d /app/oracle/install",
+    cwd => $oracle::installFiles,
     user => 'oracle',
     group => 'oinstall',
     creates => '/app/oracle/install/database',
     provider => 'shell',
   } ->
-  
+
   file {'/app/oracle/install/db_install.rsp':
     owner => 'oracle',
     group => 'oinstall',
     mode => 0640,
     content => template('oracle/db_install.rsp'),
   } ->
-  
+
   exec {'install_oracle':
     command => '/app/oracle/install/database/runInstaller -silent -ignoreSysPrereqs -ignorePrereq -noconfig -waitforcompletion -responseFile /app/oracle/install/db_install.rsp',
     cwd => '/app/oracle/install/database',
@@ -38,14 +38,14 @@ class oracle::installer {
     provider => 'shell',
     timeout => 0,
   } ->
-  
+
   exec {'post_install_root':
     command => '/app/oracle/local/product/11.2.0.1.0/db_in_box/root.sh',
     user => 'root',
     group => 'root',
     provider => 'shell',
   } ->
-  
+
   exec {"post_install_cleanup":
     command => 'rm -Rf /app/oracle/install/database/*',
     user => 'root',
